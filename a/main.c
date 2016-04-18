881f8f3c15b13362b3b63c8e6493f370cb3c1376
851166449@qq.com <851166449@qq.com>
2016-04-18 13:55:22 +0800

    hjl

---
+++ a/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  a
+//
+//  Created by administrator on 15/10/14.
+//  Copyright (c) 2015年 administrator. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
